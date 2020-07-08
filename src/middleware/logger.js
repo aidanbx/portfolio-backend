4b0a7ec29082d83bbeef23132bfb1296321ba2b0
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-08 14:13:28 -0700

    logger middleware set up

---
+++ src/middleware/logger.js
@@ -0,0 +1,12 @@
+const moment = require('moment');
+
+const logger = (req, res, next) => {
+  console.log(
+    `${moment().format()}: ${req.method} -> ${req.protocol}://${req.get(
+      'host'
+    )}${req.originalUrl}`
+  );
+  next();
+};
+
+module.exports = logger;
