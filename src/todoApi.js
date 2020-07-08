b0e3a60f565aee4e3f1c12af5e22189286b9bebf
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-08 14:14:12 -0700

    queries set up, todo API uses a router now

---
+++ src/todoApi.js
@@ -0,0 +1,59 @@
+const dotenv = require('dotenv').config({ path: '../' });
+const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const logger = require('./middleware/logger');
+const app = express();
+
+// app.use(express.static(path.join(__dirname, 'src')));
+app.use(
+  bodyParser.urlencoded({
+    extended : true
+  })
+);
+
+app.use(bodyParser.json());
+
+app.use(logger);
+
+app.use('/api/todos', require('../routes/api/todos'));
+
+app.use((req, res, next) => {
+  res.set({
+    'Content-Type'                 : 'text/json',
+    'Access-Control-Allow-Origin'  : '*',
+    'Access-Control-Allow-Headers' :
+      'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS, PUT, DELETE'
+  });
+  next();
+});
+
+app.get('/', (req, res, next) => {
+  res.status(400).json({
+    err : 'API calls must be in the form /api/<path>'
+  });
+  return;
+});
+
+app.get('/api/', (req, res, next) => {
+  res.send(
+    JSON.stringify(
+      {
+        info     : 'Todolist API',
+        commands : {
+          getTodos    : 'get /todos',
+          getTodoById : 'get /todos/:id',
+          createTodo  : 'post /todos',
+          updateTodo  : 'put /todos/:id',
+          deleteTodo  : 'delete /todos/:id'
+        }
+      },
+      null,
+      2
+    )
+  );
+});
+
+module.exports = app;
