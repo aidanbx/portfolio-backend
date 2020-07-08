c567701b13535e12242cb7e3684cdc4e188587ef
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-08 14:14:29 -0700

    added routing

---
+++ routes/api/todos.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../src/queries.js');
+
+router.get('/', db.getTodos);
+router.get('/:id', db.getTodoById);
+router.post('/', db.createTodo);
+router.put('/new/:id', db.updateTodo);
+router.put('/:id', db.toggleTodo);
+router.delete('/:id', db.deleteTodo);
+
+module.exports = router;
