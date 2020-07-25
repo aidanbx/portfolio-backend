199fa4965d21b648bcd41e38c653d6c84ea84843
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-24 20:39:27 -0700

    cors all origins

--- src/todoApi.js
+++ src/todoApi.js
@@ -7,7 +7,13 @@ const logger = require('./middleware/logger');
 const todoRoutes = require('./routes/todos');
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'src')));
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', [ '*' ]);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(
   bodyParser.urlencoded({
     extended : true,
@@ -20,16 +26,16 @@ app.use(logger);
 
 app.use('/api/todos', todoRoutes);
 
-app.use((req, res, next) => {
-  res.set({
-    'Content-Type'                 : 'text/json',
-    'Access-Control-Allow-Origin'  : '*',
-    'Access-Control-Allow-Headers' :
-      'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS, PUT, DELETE',
-  });
-  next();
-});
+// app.use('/*', (req, res, next) => {
+//   res.set({
+//     'Content-Type'                 : 'text/json',
+//     'Access-Control-Allow-Origin'  : '*',
+//     'Access-Control-Allow-Headers' :
+//       'Origin, X-Requested-With, Content-Type, Accept',
+//     'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS, PUT, DELETE',
+//   });
+//   next();
+// });
 
 app.get('/', (req, res, next) => {
   res.status(400).json({
