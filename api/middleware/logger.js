e3ff6d35e506ef9cdffafa8b9643d4a54ef87a5a
Aidan Barbieux <abarbieu@calpoly.edu>
2020-08-17 23:52:30 -0700

    obj logger

--- api/middleware/logger.js
+++ api/middleware/logger.js
@@ -1,53 +1,60 @@
 const moment = require('moment-timezone');
 const geoip = require('geoip-lite');
 const fs = require('fs');
-const logFile = './logs.txt';
+const logFile = './logs.JSON';
+const logs = require(logFile);
 const ipRegex = /[\d\.]+$/;
 
-const getLog = (req) => {
-  let clientIP;
-  try {
-    clientIP = req.headers['x-real-ip'].match(ipRegex)[0];
-  } catch (err) {
-    clientIP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress)
-      .match(ipRegex)[0];
-  }
-  if (clientIP) {
-    try {
-      console.log(clientIP);
-      const geo = geoip.lookup(clientIP);
-      // console.log(clientIP);
-      const log = `${moment().tz(
-        'America/Los_Angeles'
-      )} : ${geo.country} ${geo.region} ${geo.city} : ${req.headers[
-        'x-real-ip'
-      ]}`;
-      return log;
-    } catch (err) {
-      return clientIP;
-    }
-  }
-  return "Couldn't get IP addr";
+const getLog = (ip) => {
+	return logs[ip];
+};
+
+const makeLog = (req) => {
+	let clientIP;
+	let log = {};
+	try {
+		clientIP = req.headers['x-real-ip'].match(ipRegex)[0];
+	} catch (err) {
+		log[err.name] = err;
+		clientIP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).match(ipRegex)[0];
+	}
+	if (clientIP) {
+		log.ip = clientIP;
+		try {
+			log.geo = geoip.lookup(clientIP);
+			log.time = moment().tz('America/Los_Angeles');
+		} catch (err) {
+			log[err.name] = err;
+		}
+	} else {
+		log.ip = undefined;
+	}
+	log.url = req.originalUrl;
+	log.domain = req.hostname;
+	log.protocol = req.protocol;
+	log.path = req.path;
+	log.subdomains = req.subdomains;
+	log.method = method;
+	return log;
 };
 
 const logger = (req, res, next) => {
-  if (req.originalUrl === '/' || req.originalUrl === '/index.html') {
-    try {
-      const log = getLog(req);
+	const log = makeLog(req);
 
-      fs.appendFile(logFile, log + '\n', 'utf8', (err) => {
-        if (err) {
-          throw err;
-        } else {
-          console.log(log);
-        }
-      });
-    } catch (error) {
-      console.error(error);
-      next();
-    }
-  }
-  next();
+	try {
+		logs[log.ip] = log;
+		fs.writeFile(logFile, JSON.stringify(log, null, 2), 'utf8', (err) => {
+			if (err) {
+				throw err;
+			} else {
+				console.log(log);
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		next();
+	}
+	next();
 };
 
-module.exports = { getLog, logger };
+module.exports = { getLog, makeLog, logger };
