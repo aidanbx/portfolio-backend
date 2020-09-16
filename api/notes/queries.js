d0a7cb8ddd215d071f30b933d7a78c8974b89728
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-15 19:26:28 -0700

    sql errors fixed

--- api/notes/queries.js
+++ api/notes/queries.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: '../' });
+const axios = require('axios');
 const moment = require('moment');
 const { Pool } = require('pg');
 
@@ -10,9 +11,11 @@ const notesTable = process.env.NOTE_TABLE || 'notes';
 const getNotes = (req, res) => {
   pool.query(`SELECT * FROM ${notesTable} ORDER BY date ASC`, (err, result) => {
     if (err) {
+      console.error(err);
       res.status(500).json({
-        msg : '500 Database Down Error',
-        err : { name: err.name, message: err.message },
+        msg     : '500 Database Error',
+        err     : err,
+        summary : { name: err.name, message: err.message },
       });
       return;
     }
@@ -23,7 +26,6 @@ const getNotes = (req, res) => {
 // GET /api/notes/:id gets note with id :id
 const getNoteById = (req, res) => {
   const id = parseInt(req.params.id);
-
   if (Number.isNaN(id)) {
     res.status(400).json({ err: 'BAD REQUEST: id must be an int' });
     return;
@@ -33,14 +35,15 @@ const getNoteById = (req, res) => {
       [ id ],
       (err, result) => {
         if (err) {
+          console.error(err);
           res.status(500).json({
-            msg : 'Database Error',
-            err : { name: err.name, message: err.message },
+            msg     : '500 Database Error',
+            err     : err,
+            summary : { name: err.name, message: err.message },
           });
           return;
         }
         res.status(200).json(result.rows[0]);
-        next();
       }
     );
   }
@@ -58,9 +61,11 @@ const createNote = (req, res) => {
     [ title, content, date, archived ],
     (err, result) => {
       if (err) {
+        console.error(err);
         res.status(500).json({
-          msg : '500 Database Error',
-          err : { name: err.name, message: err.message },
+          msg     : '500 Database Error',
+          err     : err,
+          summary : { name: err.name, message: err.message },
         });
         return;
       }
@@ -83,9 +88,11 @@ const deleteNote = (req, res) => {
       [ id ],
       (err, result) => {
         if (err) {
+          console.error(err);
           res.status(500).json({
-            msg : '500 Database Error',
-            err : { name: err.name, message: err.message },
+            msg     : '500 Database Error',
+            err     : err,
+            summary : { name: err.name, message: err.message },
           });
         }
 
@@ -104,42 +111,59 @@ const updateNote = (req, res) => {
   let oldNote;
   let { title, content, date, archived } = req.body;
 
+  // console.log('IN API.... DELETE THIS LOG');
+
   if (Number.isNaN(id)) {
     res.status(400).json({ err: 'BAD REQUEST: id must be an int' });
     return;
   } else {
-    pool.query(
-      `SELECT * FROM ${notesTable} WHERE id=$1`,
-      [ id ],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({
-            msg : 'Database Error',
-            err : { name: err.name, message: err.message },
-          });
-          return;
+    axios
+      .get(
+        `http://${process.env.HOST || 'localhost'}:${process.env.PORT ||
+          '54321'}/api/notes/${id}`
+      )
+      .then((result) => {
+        // console.log(`SENT REQ FOR ${id}, `, result.data);
+
+        if (result.err) {
+          throw result.err;
         }
-        oldNote = result.rows[0];
+        // console.log('given: ', title, content, date, archived);
+
+        oldNote = result.data;
         title = title || oldNote.title;
         content = content || oldNote.content;
-        date = date || moment().unix();
-        archived = archived || oldNote.archived;
-      }
-    );
-    pool.query(
-      `UPDATE ${notesTable} SET title = ($1), content = ($2), date = ($3), archived = ($4) RETURNING *`,
-      [ title, content, date, archived ],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({
-            msg : '500 Database Error',
-            err : { name: err.name, message: err.message },
-          });
-          return;
-        }
-        res.status(200).json(result.rows[0]);
-      }
-    );
+        date = moment().unix();
+        archived = archived;
+
+        // console.log('putting with: ', title, content, date, archived);
+
+        pool.query(
+          `UPDATE ${notesTable} SET title = ($1), content = ($2), date = ($3), archived = ($4) WHERE id = ($5) RETURNING *`,
+          [ title, content, date, archived, id ],
+          (err, result) => {
+            if (err) {
+              console.error(err);
+              result.status(500).json({
+                msg     : '500 Database Error',
+                err     : err,
+                summary : { name: err.name, message: err.message },
+              });
+              return;
+            }
+            res.status(200).json(result.rows[0]);
+          }
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({
+          msg     : '500 Database Error',
+          err     : err,
+          summary : { name: err.name, message: err.message },
+        });
+        return;
+      });
   }
 };
 
