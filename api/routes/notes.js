e0646c75650772daa9ad6128cd83f38f5702b7c7
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-17 22:49:26 -0700

    Email format, notes 'security'

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../notes/queries.js');
+const logger = require('../middleware/logger.js');
 
+router.get('/allowed', (req, res) => {
+  console.log(logger.getIP(req));
+  if (
+    logger.getIP(req) === '38.142.236.234' ||
+    logger.getIP(req) === '192.168.4.41' ||
+    logger.getIP(req) === '1'
+  ) {
+    res.status(200).send(true);
+  } else {
+    res.status(403).send(false);
+  }
+});
 router.get('/', db.getNotes);
 router.get('/:id', db.getNoteById);
 router.post('/', db.createNote);
