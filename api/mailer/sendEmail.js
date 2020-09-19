58d41629b2f77d641571027aee4301a46aef5d43
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-18 17:06:48 -0700

    notes demo

--- api/mailer/sendEmail.js
+++ api/mailer/sendEmail.js
@@ -1,160 +1,165 @@
-require('dotenv').config({ path: '../' });
-const nodemailer = require('nodemailer');
-const logger = require('../middleware/logger');
+require("dotenv").config({ path: "../" });
+const nodemailer = require("nodemailer");
+const logger = require("../middleware/logger");
 const clid = process.env.CLIENTID;
 const clsec = process.env.CLIENTSECRET;
 const refrtok = process.env.REFRESHTOKEN;
 const from = process.env.FROMEMAIL;
 const to = process.env.TOEMAIL;
-const logs = require('../../logs.json');
+const logs = require("../../logs.json");
 
 var transporter = nodemailer.createTransport({
-  host   : 'smtp.gmail.com',
-  port   : 465,
-  secure : true,
-  auth   : {
-    type         : 'OAuth2',
-    user         : from,
-    clientId     : clid,
-    clientSecret : clsec,
-    refreshToken : refrtok,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: from,
+    clientId: clid,
+    clientSecret: clsec,
+    refreshToken: refrtok,
   },
 });
 
 sendEmail = async (req, res) => {
   let { replyto, subject, name, content, attachments } = req.body;
   const log = logs[logger.getIP(req)];
-  if (!replyto) replyto = 'No Return Address';
-  if (!subject) subject = 'No Subject';
-  if (!name) name = 'No Name Given';
-  if (!content) content = 'No Content';
+  if (!replyto) replyto = "No Return Address";
+  if (!subject) subject = "No Subject";
+  if (!name) name = "No Name Given";
+  if (!content) content = "No Content";
 
   transporter.sendMail(
     {
-      from        : `"${name}" <mailer@barbieux.dev>`,
-      to          : `${to}`,
-      subject     : `${subject}`,
-      html        : `
-<!DOCTYPE html>
+      from: `"${name}" <mailer@barbieux.dev>`,
+      to: `${to}`,
+      subject: `${subject}`,
+      html: `
+      <!DOCTYPE html>
 <html lang="en">
-
-  <head>
-    <style>
-      .Tiled-back {
+  <body style="min-height: 100vh; margin: 0px; padding: 0px">
+    <div
+      style="
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+          Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif;
         min-height: 100vh;
         background: url('https://barbieux.dev/bg-pblue-str.png');
-        overflow-x: hidden;
-        background-repeat: round;
         background-size: 64px;
-      }
+        width: 100%;
+        margin: 0px;
+        padding: 0px;
+      "
+    >
+      <table width="100%" style="border-radius: 10px; position: absolute">
+        <td valign="top" align="center">
+          <table
+            align="center"
+            cellpadding="7"
+            style="
+              margin-top: 50px;
+              max-width: 616px;
+              border-radius: 10px;
+              width: 100%;
+            "
+          >
+            <tbody
+              valign="top"
+              align="center"
+              style="
+                margin: 1px;
+                display: block;
+                border-radius: 10px 10px 0px 0px;
+                background-color: #00000041;
+              "
+            >
+              <tr style="background-color: #0000">
+                <td valign="top" align="center">
+                  <img
+                    src="https://barbieux.dev/logo512.png"
+                    alt="Sherbert"
+                    width="66px"
+                    height="66px"
+                    style="max-width: 100%"
+                  />
+                  <h2 style="color: #fdb241">${name} Sent a message</h2>
+                </td>
+              </tr>
+            </tbody>
+            <tbody
+              style="background-color: #232435; margin: 1px; color: #a0a0a0"
+            >
+              <tr>
+                <td valign="top">
+                  <strong style="color: #fdb241">name:</strong><br />
+                  <pre>${name}</pre>
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <strong style="color: #fdb241">email:</strong><br />
+                  <pre>${replyto}</pre>
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <strong style="color: #fdb241">subject:</strong><br />
+                  <pre>${subject}</pre>
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  <strong style="color: #fdb241"> message:</strong><br />
+                  <pre>${content}</pre>
+                </td>
+              </tr>
+            </tbody>
 
-      .table-header {
-        vertical-align: top;
-        font-family: "Source Code Pro", monospace;
-        padding: 11px;
-        border-bottom: 1px solid rgb(227, 227, 218);
-        font-size: 14px
-      }
-    </style>
-  </head>
-
-  <body class="Tiled-back">
-    <div bgcolor="#ffffff" height="100%" width="100%">
-
-
-      <table class="Tiled-back" style="width:100%;margin:0px auto" width="100%">
-        <tbody>
-          <tr>
-            <td valign="top" align="center" style="vertical-align:top">
-              <table style="width:100%" width="100%">
-                <tbody>
-                  <tr>
-                    <td align="center" style="vertical-align:top;color:rgb(16,55,66);padding:22px" valign="top">
-                      <div>
-                        <img src="https://barbieux.dev/logo512.png" alt="Sherbert icon" width="66px" height="66px"
-                          style="max-width:100%;margin-bottom:22px">
-                      </div>
-                      <p
-                        style="color:white;font-weight:700;font-family:Poppins,sans-serif;margin:0px;text-transform:uppercase">
-                        ${name} Sent a message</p>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <table style="width:100%;background-color:white;max-width:616px;">
-                <tbody>
-                  <tr>
-                    <td valign="top">
-                      <table style="width:100%;">
-
-                        <tbody>
-                        <tr>
-                            <td class="table-header" valign="top">
-                              <strong>name:</strong><br>
-                              <pre>${name}</pre>
-                            </td >
-                          </tr >
-                          <tr>
-                            <td class="table-header" valign="top">
-                              <strong>email:</strong><br>
-                              <pre>${replyto}</pre>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="table-header" valign="top">
-                              <strong>subject:</strong><br>
-                              <pre>${subject}</pre>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="table-header" valign="top">
-                              <strong>message:</strong><br>
-                              <pre>${content}</pre>
-                            </td>
-                          </tr>
-
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                  <tr align="center" style="padding:0px 0px 22px">
-                    <td style="padding:11px;">
-                      <span style="color:rgb(145,143,141);font-size:12px">${log
-                        .geo.country} ${log.geo.region} ${log.geo
-        .city} --- ${log.time} --- ${logger.getIP(req)}</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-        </tbody>
+            <tr
+              align="center"
+              style="
+                display: block;
+                background-color: #232435;
+                border-radius: 0px 0px 10px 10px;
+                padding: 0px 0px 22px;
+              "
+            >
+              <td style="padding-bottom: 1px">
+                <span style="color: #c48323; font-size: 12px"
+                  >${log.geo.country} ${log.geo.region} ${log.geo.city} ---
+                  ${log.time} --- ${logger.getIP(req)}</span
+                >
+              </td>
+            </tr>
+          </table>
+        </td>
       </table>
     </div>
   </body>
-
 </html>
+
+
+      
     `,
-      attachments : attachments.map((attch) => ({
-        filename : attch.filename,
-        content  : new Buffer.from(attch.raw.split('base64,')[1], 'base64'),
+      attachments: attachments.map((attch) => ({
+        filename: attch.filename,
+        content: new Buffer.from(attch.raw.split("base64,")[1], "base64"),
       })),
     },
     (err, info) => {
       if (err) {
-        console.log('ERROR SENDING MAIL');
+        console.log("ERROR SENDING MAIL");
         console.error(err);
       } else {
-        console.log('SENT MAIL!');
+        console.log("SENT MAIL!");
         res.status(200).json({
-          status      : 'Sent Email!, updated msg',
-          from        : `${log.country} ${log.region} ${log.city}`,
-          time        : `${log.time}`,
+          status: "Sent Email!, updated msg",
+          from: `${log.country} ${log.region} ${log.city}`,
+          time: `${log.time}`,
           subject,
           name,
           content,
           replyto,
-          emailid     : info,
+          emailid: info,
           attachments,
         });
       }
--- api/notes/queries.js
+++ api/notes/queries.js
@@ -1,11 +1,11 @@
-require('dotenv').config({ path: '../' });
-const axios = require('axios');
-const moment = require('moment');
-const { Pool } = require('pg');
+require("dotenv").config({ path: "../" });
+const axios = require("axios");
+const moment = require("moment");
+const { Pool } = require("pg");
 
 const pool = new Pool();
 
-const notesTable = process.env.NOTE_TABLE || 'notes';
+const notesTable = process.env.NOTE_TABLE || "notes";
 
 // GET /api/notes gets all notes in db
 const getNotes = (req, res) => {
@@ -15,9 +15,9 @@ const getNotes = (req, res) => {
       if (err) {
         console.error(err);
         res.status(500).json({
-          msg     : '500 Database Error',
-          err     : err,
-          summary : { name: err.name, message: err.message },
+          msg: "500 Database Error",
+          err: err,
+          summary: { name: err.name, message: err.message },
         });
         return;
       }
@@ -30,19 +30,19 @@ const getNotes = (req, res) => {
 const getNoteById = (req, res) => {
   const id = parseInt(req.params.id);
   if (Number.isNaN(id)) {
-    res.status(400).json({ err: 'BAD REQUEST: id must be an int' });
+    res.status(400).json({ err: "BAD REQUEST: id must be an int" });
     return;
   } else {
     pool.query(
       `SELECT * FROM ${notesTable} WHERE id = ($1)`,
-      [ id ],
+      [id],
       (err, result) => {
         if (err) {
           console.error(err);
           res.status(500).json({
-            msg     : '500 Database Error',
-            err     : err,
-            summary : { name: err.name, message: err.message },
+            msg: "500 Database Error",
+            err: err,
+            summary: { name: err.name, message: err.message },
           });
           return;
         }
@@ -54,21 +54,22 @@ const getNoteById = (req, res) => {
 
 // POST /api/Notes title=random title&complete=false
 const createNote = (req, res) => {
-  let { title, content, date, archived } = req.body;
-  title = title || 'No Title';
-  content = content || 'No Content';
+  let { title, content, date, archived, severity } = req.body;
+  title = title || "No Title";
+  content = content || "No Content";
   date = date || moment().unix();
   archived = archived || false;
+  severity = severity || 50;
   pool.query(
-    `INSERT INTO ${notesTable} ( title, content, date, archived ) VALUES ($1, $2, $3, $4) RETURNING *`,
-    [ title, content, date, archived ],
+    `INSERT INTO ${notesTable} ( title, content, date, archived, severity ) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    [title, content, date, archived, severity],
     (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).json({
-          msg     : '500 Database Error',
-          err     : err,
-          summary : { name: err.name, message: err.message },
+          msg: "500 Database Error",
+          err: err,
+          summary: { name: err.name, message: err.message },
         });
         return;
       }
@@ -83,25 +84,25 @@ const deleteNote = (req, res) => {
   const id = parseInt(req.params.id);
 
   if (Number.isNaN(id)) {
-    res.status(400).json({ err: 'BAD REQUEST: id must be an int' });
+    res.status(400).json({ err: "BAD REQUEST: id must be an int" });
     return;
   } else {
     pool.query(
       `DELETE FROM ${notesTable} WHERE id = ($1)`,
-      [ id ],
+      [id],
       (err, result) => {
         if (err) {
           console.error(err);
           res.status(500).json({
-            msg     : '500 Database Error',
-            err     : err,
-            summary : { name: err.name, message: err.message },
+            msg: "500 Database Error",
+            err: err,
+            summary: { name: err.name, message: err.message },
           });
         }
 
         res.status(200).json({
-          msg : `note deleted with id ${id}`,
-          id  : id,
+          msg: `note deleted with id ${id}`,
+          id: id,
         });
       }
     );
@@ -112,18 +113,19 @@ const deleteNote = (req, res) => {
 const updateNote = (req, res) => {
   const id = parseInt(req.params.id);
   let oldNote;
-  let { title, content, date, archived } = req.body;
+  let { title, content, date, archived, severity } = req.body;
 
   // console.log('IN API.... DELETE THIS LOG');
 
   if (Number.isNaN(id)) {
-    res.status(400).json({ err: 'BAD REQUEST: id must be an int' });
+    res.status(400).json({ err: "BAD REQUEST: id must be an int" });
     return;
   } else {
     axios
       .get(
-        `http://${process.env.HOST || 'localhost'}:${process.env.PORT ||
-          '54321'}/api/notes/${id}`
+        `http://${process.env.HOST || "localhost"}:${
+          process.env.PORT || "54321"
+        }/api/notes/${id}`
       )
       .then((result) => {
         // console.log(`SENT REQ FOR ${id}, `, result.data);
@@ -134,23 +136,24 @@ const updateNote = (req, res) => {
         // console.log('given: ', title, content, date, archived);
 
         oldNote = result.data;
-        date = title || content ? moment().unix() : oldNote.date;
+        date = date || title || content ? moment().unix() : oldNote.date;
         title = title || oldNote.title;
         content = content || oldNote.content;
+        severity = severity || oldNote.severity;
         archived = archived;
 
         // console.log('putting with: ', title, content, date, archived);
 
         pool.query(
-          `UPDATE ${notesTable} SET title = ($1), content = ($2), date = ($3), archived = ($4) WHERE id = ($5) RETURNING *`,
-          [ title, content, date, archived, id ],
+          `UPDATE ${notesTable} SET title = ($1), content = ($2), date = ($3), archived = ($4), severity = ($5) WHERE id = ($6) RETURNING *`,
+          [title, content, date, archived, severity, id],
           (err, result) => {
             if (err) {
               console.error(err);
               result.status(500).json({
-                msg     : '500 Database Error',
-                err     : err,
-                summary : { name: err.name, message: err.message },
+                msg: "500 Database Error",
+                err: err,
+                summary: { name: err.name, message: err.message },
               });
               return;
             }
@@ -161,9 +164,9 @@ const updateNote = (req, res) => {
       .catch((err) => {
         console.error(err);
         res.status(500).json({
-          msg     : '500 Database Error',
-          err     : err,
-          summary : { name: err.name, message: err.message },
+          msg: "500 Database Error",
+          err: err,
+          summary: { name: err.name, message: err.message },
         });
         return;
       });
