ab561ee6f61275d2a8553ad999bfab36a7c8426b
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-18 17:08:00 -0700

    commit on push prod

--- push-prod.sh
+++ push-prod.sh
@@ -5,6 +5,8 @@ if [ "$#" -lt 1 ]; then
 elif [ ! -d "../prod-master/" ]; then
   echo "../prod-master/ is not a valid directory"
 else
+  git add ./api/
+  git commit -m "$1"
   cp -r ./api ../prod-master/
   cd ../prod-master
   git add ./api
