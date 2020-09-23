d204c8b1fe0f70f22a071ea83982a967d4d0023a
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-23 13:11:19 -0700

    aidanb

--- api/api.js
+++ api/api.js
@@ -1,41 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('./middleware/logger');
-const notesRoutes = require('./routes/notes');
-const mailRoutes = require('./routes/mail');
-const logs = require('../logs.json');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("./middleware/logger");
+const notesRoutes = require("./routes/notes");
+const mailRoutes = require("./routes/mail");
+const logs = require("../logs.json");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const IP = process.env.IP || 'localhost';
+const IP = process.env.IP || "localhost";
 
-app.set('PORT', PORT);
-app.set('IP', IP);
+app.set("PORT", PORT);
+app.set("IP", IP);
 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', [ '*' ]);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
 
   next();
 });
 
 //! redirect
-app.use('/*', (req, res, next) => {
+app.use("/*", (req, res, next) => {
   const log = logger.makeLog(req);
-  if (log.domain === 'abarbieux.com' || log.domain === 'www.abarbieux.com') {
-    res.redirect(301, 'https://barbieux.dev' + log.url);
+  if (
+    log.domain === "abarbieux.com" ||
+    log.domain === "www.abarbieux.com" ||
+    log.domain === "aidanb.me" ||
+    log.domain === "www.aidanb.me"
+  ) {
+    res.redirect(301, "https://barbieux.dev" + log.url);
   }
   next();
 });
 
 app.use(
   bodyParser.urlencoded({
-    extended : true,
+    extended: true,
   })
 );
 
-app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.json({ limit: "500mb" }));
 
 const logWrapper = (req, res, next) => {
   return logger.logger(req, res, next, logs);
@@ -43,22 +48,22 @@ const logWrapper = (req, res, next) => {
 
 app.use(logWrapper);
 
-app.use('/api/notes', notesRoutes);
-app.use('/api/mail', mailRoutes);
+app.use("/api/notes", notesRoutes);
+app.use("/api/mail", mailRoutes);
 // app.set('json spaces', 2);
-app.set('view options', { pretty: true });
-app.get('/api/', (req, res, next) => {
-  res.header('Content-Type', 'application/json');
+app.set("view options", { pretty: true });
+app.get("/api/", (req, res, next) => {
+  res.header("Content-Type", "application/json");
   res.status(200).send(
     JSON.stringify(
       {
-        title            : 'barbieux.dev REST API',
-        todoListCommands : {
-          prefix : '/api/',
+        title: "barbieux.dev REST API",
+        todoListCommands: {
+          prefix: "/api/",
         },
-        mailCommands     : {
-          sendMail :
-            'post /mail/?replyto=theirEmail&name=theirName&subject=&content=whatTheyTyped',
+        mailCommands: {
+          sendMail:
+            "post /mail/?replyto=theirEmail&name=theirName&subject=&content=whatTheyTyped",
         },
       },
       null,
