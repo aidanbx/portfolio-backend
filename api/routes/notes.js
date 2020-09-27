7ac31df0cc65b2f1d50eebaf3d57800715298be2
Aidan Barbieux <abarbieu@calpoly.edu>
2020-09-27 15:42:05 -0700

    added SB IP

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../notes/queries.js");
-const logger = require("../middleware/logger.js");
+const db = require('../notes/queries.js');
+const logger = require('../middleware/logger.js');
 
-router.get("/allowed", (req, res) => {
+router.get('/allowed', (req, res) => {
   console.log(logger.getIP(req));
+  const reqIP = logger.getIP(req);
   if (
-    logger.getIP(req) === "38.142.236.234" ||
-    logger.getIP(req) === "192.168.4.41" ||
-    logger.getIP(req) === "1"
+    reqIP === '38.142.236.234' ||
+    reqIP === '192.168.4.41' ||
+    reqIP === '68.6.89.27' ||
+    reqIP === '1'
   ) {
     res.status(200).send(true);
   } else {
     res.status(403).send(false);
   }
 });
-router.get("/", db.getNotes);
-router.get("/:id", db.getNoteById);
-router.post("/", db.createNote);
-router.put("/:id", db.updateNote);
-router.delete("/:id", db.deleteNote);
+router.get('/', db.getNotes);
+router.get('/:id', db.getNoteById);
+router.post('/', db.createNote);
+router.put('/:id', db.updateNote);
+router.delete('/:id', db.deleteNote);
 
 module.exports = router;
